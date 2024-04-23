d38b8fe0b5f30b7e1b4e9fa5d1f317880ea6274d
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-23 07:01:14 -0600

    First Commit

---
+++ Prueba/ArchivoMain.swift
@@ -0,0 +1,18 @@
+//
+//  ArchivoMain.swift
+//  Prueba
+//
+//  Created by Andres Cabral on 23/04/24.
+//
+
+import SwiftUI
+
+struct ArchivoMain: View {
+    var body: some View {
+        Text("Página Main")
+    }
+}
+
+#Preview {
+    ArchivoMain()
+}
--- Prueba/ContentView.swift
+++ Prueba/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Esta es una prueba página 1")
         }
         .padding()
     }
