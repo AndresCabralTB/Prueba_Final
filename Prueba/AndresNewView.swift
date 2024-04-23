cd2545f74c127e8c50f4bf53d36ec86d269f7c48
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-23 07:13:22 -0600

    Andres New View

---
+++ Prueba/AndresNewView.swift
@@ -0,0 +1,18 @@
+//
+//  AndresNewView.swift
+//  Prueba
+//
+//  Created by Andres Cabral on 23/04/24.
+//
+
+import SwiftUI
+
+struct AndresNewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AndresNewView()
+}
