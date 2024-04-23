444493ead06b2ef4974bb51cf8123a96a6d4c517
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-23 07:05:04 -0600

    My changes

---
+++ Prueba/AndresView.swift
@@ -0,0 +1,18 @@
+//
+//  AndresView.swift
+//  Prueba
+//
+//  Created by Andres Cabral on 23/04/24.
+//
+
+import SwiftUI
+
+struct AndresView: View {
+    var body: some View {
+        Text("Andres View")
+    }
+}
+
+#Preview {
+    AndresView()
+}
