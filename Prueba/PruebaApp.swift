47ae33ad6662d9c7ed8f6d73fc6643b9929d0219
AndresCabralTB <andres.cabralrs@udlap.mx>
2024-04-23 06:59:54 -0600

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Andres Cabral on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
